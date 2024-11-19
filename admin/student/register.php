9629c0ef61a34122f36f5c6f7aba3b5d693af34c
raniel-roque <raniel.roque0509@gmail.com>
2024-11-20 07:34:06 +0800

    Added UI

--- admin/student/register.php
+++ admin/student/register.php
@@ -16,8 +16,8 @@
     require '../partials/side-bar.php';
 ?>
 
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
-    <h1 class="h2">Register a New Subject</h1>
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">
+    <h1 class="h2">Register a New Student</h1>
     <div class="mt-5 mb-3 w-100">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0">
@@ -25,6 +25,67 @@
                 <li class="breadcrumb-item active" aria-current="page">Register Student</li>
             </ol>
         </nav>
+    </div>
+
+    <?php if (!empty($arrErrors)): ?>
+        <?= displayErrors($arrErrors); ?>
+    <?php endif; ?>
+
+    <form method="POST" action="" class="border border-secondary-1 p-5 mb-4">
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="txtStudentID" name="txtStudentID" placeholder="Student ID" value="<?= isset($student_id) ? $student_id : '' ?>">
+            <label for="txtStudentID">Student ID</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="txtFirstName" name="txtFirstName" placeholder="First Name" value="<?= isset($first_name) ? $first_name : '' ?>">
+            <label for="txtFirstName">First Name</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="txtLastName" name="txtLastName" placeholder="Last Name" value="<?= isset($last_name) ? $last_name : '' ?>">
+            <label for="txtLastName">Last Name</label>
+        </div>
+
+        <button name="btnRegister" type="submit" class="btn btn-primary w-100">Register Student</button>
+    </form>
+
+    <div class="mt-3">
+        <div class="border border-secondary-1 p-5 mb-4">
+            <h5>Student List</h5>
+            <hr>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Student ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Option</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <form method="POST" action="edit.php" class="d-inline">
+                                    <input type="hidden" name="student_id">
+                                    <button type="submit" name="btnEdit" class="btn btn-primary btn-sm">Edit</button>
+                                </form>
+
+                                <form method="POST" action="delete.php" class="d-inline">
+                                    <input type="hidden" name="student_id">
+                                    <button type="submit" name="btnDelete" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <tr>
+                        <td colspan="4" class="text-center">No students found</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </main>
 
