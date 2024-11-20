37b672fd963f603576f159d95e028cc81d6a4ab5
raniel-roque <raniel.roque0509@gmail.com>
2024-11-20 08:26:52 +0800

    Added Template Files

---
+++ admin/student/delete.php
@@ -0,0 +1,33 @@
+<?php
+    ob_start();
+    session_start();
+    $title = 'Register Student';
+    $_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
+
+    require '../../functions.php';
+    guard();
+
+    $pathDashboard = "../dashboard.php";
+    $pathLogout = "../logout.php";
+    $pathSubjects = "../subject/add.php";
+    $pathStudents = "register.php";
+
+    require '../partials/header.php';
+    require '../partials/side-bar.php';
+?>
+
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">
+    <h1 class="h2">Delete Student</h1>
+    <div class="mt-5 mb-3 w-100">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="../dashboard.php" class="text-decoration-none">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="register.php" class="text-decoration-none">Register Student</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Delete Student</li>
+            </ol>
+        </nav>
+    </div>
+
+</main>
+
+<?php require '../partials/footer.php'; ?>
