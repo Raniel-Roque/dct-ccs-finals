735caf5429ad5419a69b87fc63bc51f6d1dd2dca
raniel-roque <raniel.roque0509@gmail.com>
2024-11-20 09:42:37 +0800

    Fixed breadcrumb margin

--- admin/student/dettach-subject.php
+++ admin/student/dettach-subject.php
@@ -17,7 +17,7 @@
 
 <main class="container justify-content-between align-items-center col-8 mt-4">
     <h2 class="mt-4">Attach Subject to Student</h2>
-    <div class="mt-4">
+    <div class="mt-5 mb-3 w-100">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item"><a href="../dashboard.php" class="text-decoration-none">Dashboard</a></li>
