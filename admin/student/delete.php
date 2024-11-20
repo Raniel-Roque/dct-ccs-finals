37e02410be441fb01d72f46110277e21fcf36a9e
raniel-roque <raniel.roque0509@gmail.com>
2024-11-20 08:38:14 +0800

    Added UI and Functionality

--- admin/student/delete.php
+++ admin/student/delete.php
@@ -1,7 +1,7 @@
 <?php
     ob_start();
     session_start();
-    $title = 'Register Student';
+    $title = 'Delete Student';
     $_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
 
     require '../../functions.php';
@@ -14,6 +14,44 @@
 
     require '../partials/header.php';
     require '../partials/side-bar.php';
+
+    if (isset($_POST['student_id'])) {
+        $student_id = $_POST['student_id'];
+
+        $con = getDatabaseConnection();
+        $stmt = $con->prepare("SELECT * FROM students WHERE student_id = ?");
+        $stmt->bind_param("s", $student_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $student = $result->fetch_assoc();
+        $stmt->close();
+        mysqli_close($con);
+
+        if (!$student) {
+            header("Location: register.php");
+            exit;
+        }
+    } else {
+        header("Location: register.php");
+        exit;
+    }
+
+    if (isset($_POST['btnConfirmDelete'])) {
+        $con = getDatabaseConnection();
+        $stmt = $con->prepare("DELETE FROM students WHERE student_id = ?");
+        $stmt->bind_param("s", $student_id);
+        $stmt->execute();
+        $stmt->close();
+        mysqli_close($con);
+
+        header("Location: register.php");
+        exit;
+    }
+
+    if (isset($_POST['btnCancel'])) {
+        header("Location: register.php");
+        exit;
+    }
 ?>
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">
@@ -28,6 +66,22 @@
         </nav>
     </div>
 
+    <form method="POST" action="" class="border border-secondary-1 p-5 mb-4">
+        <p>Are you sure you want to delete the following student record?</p>
+
+        <ul>
+            <li><strong>Student ID:</strong> <?= htmlspecialchars($student['student_id']); ?> </li>
+            <li><strong>First Name:</strong> <?= htmlspecialchars($student['first_name']); ?> </li>
+            <li><strong>Last Name:</strong> <?= htmlspecialchars($student['last_name']); ?> </li>
+        </ul>
+
+        <input type="hidden" name="student_id" value="<?= htmlspecialchars($student['student_id']); ?>">
+
+        <div>
+            <button name="btnCancel" type="submit" class="btn btn-secondary">Cancel</button>
+            <button name="btnConfirmDelete" type="submit" class="btn btn-danger">Delete Student Record</button>
+        </div>
+    </form>
 </main>
 
 <?php require '../partials/footer.php'; ?>
